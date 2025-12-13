2523b40e373f85dffa518c9d0afadd6686347b94
wfsn1426-blip <w.f.s.n1426@gmail.com>
2025-12-13 09:13:11 +0300

    Create Report.js

---
+++ Report.js
@@ -0,0 +1,14 @@
+function Report() {
+  return (
+    <div>
+      <h2>رفع بلاغ</h2>
+
+      <input type="text" placeholder="نوع البلاغ" /><br /><br />
+      <textarea placeholder="وصف البلاغ"></textarea><br /><br />
+
+      <button>إرسال البلاغ</button>
+    </div>
+  );
+}
+
+export default Report;
